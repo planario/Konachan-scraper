abd866da3081ae1a1de27159cc1f28d7fbe87edd
planario <32709294+planario@users.noreply.github.com>
2023-02-28 14:36:00 -0300

    Updating the code
    fixed the progress bar that was not progressing and giving a not realistic percentage

--- konadownloader.py
+++ konadownloader.py
@@ -2,7 +2,9 @@ import os
 import requests
 from bs4 import BeautifulSoup
 import sys
-import concurrent.futures
+from tqdm import tqdm
+from concurrent.futures import ThreadPoolExecutor
+
 
 tag = input("Enter tag: ")
 url = f"https://konachan.com/post?tags={tag}"
@@ -18,40 +20,41 @@ if not os.path.exists(folder_path):
 page_number = 1
 downloaded_images = set()
 
-while True:
-    response = requests.get(url + "&page=" + str(page_number), headers=headers)
-    soup = BeautifulSoup(response.content, "html.parser")
 
-    image_elements = soup.find_all("a", class_="directlink largeimg")
+def download_image(image_url):
+    image_name = image_url.split("/")[-1]
+    image_path = os.path.join(folder_path, image_name)
 
-    if not image_elements:
-        break
+    if image_name not in downloaded_images:
+        with open(image_path, "wb") as f:
+            f.write(requests.get(image_url).content)
 
-    with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
-        for image_element in image_elements:
-            image_url = image_element["href"]
-            image_name = image_url.split("/")[-1]
-            image_path = os.path.join(folder_path, image_name)
+        downloaded_images.add(image_name)
+    else:
+        print(f"{image_name} already downloaded. Skipping...")
 
-            if image_name not in downloaded_images:
-                def download_image(image_url, image_path):
-                    response = requests.get(image_url, stream=True, headers=headers)
-                    with open(image_path, "wb") as f:
-                        for chunk in response.iter_content(chunk_size=1024):
-                            if chunk:
-                                f.write(chunk)
 
-                executor.submit(download_image, image_url, image_path)
+with ThreadPoolExecutor(max_workers=5) as executor:
+    while True:
+        response = requests.get(url + "&page=" + str(page_number), headers=headers)
+        soup = BeautifulSoup(response.content, "html.parser")
 
-                downloaded_images.add(image_name)
-            else:
-                print(f"{image_name} already downloaded. Skipping...")
+        image_elements = soup.find_all("a", class_="directlink largeimg")
 
-    page_number += 1
+        if not image_elements:
+            break
 
-    # Calculate progress percentage and print page counter
-    progress_percentage = round((page_number / 100) * len(image_elements), 2)
-    sys.stdout.write(f"\rDownloading page {page_number} ({progress_percentage}%).")
-    sys.stdout.flush()
+        image_urls = [image_element["href"] for image_element in image_elements]
+
+        with tqdm(total=len(image_elements), desc=f"Downloading page {page_number}") as progress_bar:
+            futures = []
+            for image_url in image_urls:
+                future = executor.submit(download_image, image_url)
+                future.add_done_callback(lambda p: progress_bar.update())
+                futures.append(future)
+            for future in futures:
+                future.result()
+
+        page_number += 1
 
 print(f"\nFinished downloading all images for {subfolder_name}.")
